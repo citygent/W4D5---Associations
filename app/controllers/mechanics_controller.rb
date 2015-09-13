94f862dae0844473d0edbb17e20e8dea1fe92416
James White <cyberyt@gmail.com>
2015-09-13 20:05:10 +0100

    Mechanic Views and Controller Working - ISSUE: Still can't delete, same fkey

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -1,2 +1,50 @@
 class MechanicsController < ApplicationController
+  def index
+    @mechanics = Mechanic.all
+  end
+
+  def new
+    @mechanic = Mechanic.new
+  end
+
+  def create
+    mech = Mechanic.new(mech_params)
+
+    if mech.save
+      redirect_to mech
+    else
+      render :new
+    end
+
+  end
+
+  def show 
+    @mechanic = Mechanic.find(params[:id])
+  end
+
+  def edit 
+    @mechanic = Mechanic.find(params[:id])
+  end
+
+  def update 
+    mech = Mechanic.find(params[:id])
+
+    if mech.update(mech_params)
+      redirect_to mech
+    else
+      render :edit
+    end
+  end
+
+  def destroy 
+    mech = Mechanic.find(params[:id]).destroy
+
+    redirect_to mechanics_path
+  end
+
+  private
+  def mech_params
+    params.require(:mechanic).permit(:name, :shop)
+  end
+
 end
